7feac02208bfdda13dcaeff1ee7ea2cf9a2655a8
mconcato <31354991+mconcato@users.noreply.github.com>
2017-09-03 13:00:36 -0400

    Add files via upload

--- Program/passwords.py
+++ Program/passwords.py
@@ -1,24 +1,27 @@
 """
 Michael Concato
 Information and password storage for REDCap and MySQL
-
+ 
 """
 
 
 Mysql_Tables = dict()
 
 
-# To add new MySQL database, copy and past next line below last MySQL database above, and fill in
+"""
+To add new MySQL database, copy and past next line below last MySQL database above, and fill in
 Mysql_Tables[''] = ["", "", ""]
-# Mysql_Tables[(database name] = [(user), (password), (server name)]
-
+Mysql_Tables[(database name] = [(user), (password), (server name)]
+"""
 
 REDCap_Projects = dict()
 
-# To create new, copy and paste next line below last REDCap project aboce, and fill in accordingly
-REDCap_Projects[''] = ["",""]
-# REDCap_Projects[(project name] = [(project url), (project token)]
 
+"""
+To create new, copy and paste next line below last REDCap project aboce, and fill in accordingly
+REDCap_Projects[''] = ["",""]
+REDCap_Projects[(project name] = [(project url), (project token)]
+"""
 
 def valid_mysql(database):
     return Mysql_Tables.get(database) is not None
